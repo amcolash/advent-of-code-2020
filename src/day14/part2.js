9842660ba0364d1f5e2dae5aad7529d4173a82a4
Andrew McOlash <amcolash@gmail.com>
2020-12-16 22:55:02 -0600

    finish day 14 part 2

--- src/day14/part2.js
+++ src/day14/part2.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const input = fs.readFileSync(path.join(__dirname, 'input3.txt')).toString().trim();
+const input = fs.readFileSync(path.join(__dirname, 'input.txt')).toString().trim();
 
 const instructions = input.split('\n').map((i) => {
   const maskMatch = i.match(/mask = ([\d\w]+)/);
@@ -10,36 +10,37 @@ const instructions = input.split('\n').map((i) => {
   if (memMatch) return { i: 'mem', v1: Number.parseInt(memMatch[1]), v2: Number.parseInt(memMatch[2]) };
 });
 
-// From https://stackoverflow.com/a/61352745/2303432
-function findNthOccurence(string, nth, char) {
-  let index = 0;
-  for (let i = 0; i < nth; i += 1) {
-    if (index !== -1) index = string.indexOf(char, index + 1);
+// From https://stackoverflow.com/a/14482123/2303432
+function nthIndex(str, pat, n) {
+  var L = str.length,
+    i = -1;
+  while (n-- && i++ < L) {
+    i = str.indexOf(pat, i);
+    if (i < 0) break;
   }
-  return index;
+  return i;
 }
 
 function generateAddresses(a) {
   const mask1Bin = mask.replace(/X/g, 0);
   const maskedA = BigInt(a) | BigInt('0b' + mask1Bin);
-  console.log(a.toString(2).padStart(36, '0'));
-  console.log(mask);
 
   const numFloating = (mask.match(/X/g) || []).length;
   const variations = Math.pow(2, numFloating);
-  const addresses = new BigUint64Array(variations);
+  const addresses = [];
 
   for (let i = 0; i < variations; i++) {
     let tempA = maskedA;
 
     for (let j = 0; j < numFloating; j++) {
-      const floatingIndex = mask.length - findNthOccurence(mask, j + 1, 'X') - 1;
+      const floatingIndex = BigInt(mask.length - nthIndex(mask, 'X', j + 1) - 1);
 
       // if bit is set to 1
       if ((i >> j) & 1) {
-        tempA |= BigInt(1 << floatingIndex);
+        tempA |= BigInt(1n << floatingIndex);
       } else {
-        tempA &= BigInt(~(1 << floatingIndex));
+        // bit is set to 0
+        tempA &= ~BigInt(1n << floatingIndex);
       }
     }
 
@@ -66,7 +67,4 @@ instructions
 let sum = 0;
 Object.values(mem).forEach((m) => (sum += m));
 
-console.log(mem);
 console.log(sum);
-
-// too low 3996554650354
